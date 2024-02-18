cc42721e37d37d5c4e0c147e670cea9addf4c3a6
joao.lamao <joao.lamao@ebinterchange.com.br>
2024-02-17 22:15:49 -0300

    feat: init project

---
+++ src/main/java/com/inine/api3sem/Api3semApplication.java
@@ -0,0 +1,13 @@
+package com.inine.api3sem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api3semApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api3semApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inine/api3sem/Api3semApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inine.api3sem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api3semApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
